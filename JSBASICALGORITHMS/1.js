c116c604407294c5464db9305097645237f2982a
abelgh91 <abelgh91@gmail.com>
2023-09-26 15:47:51 +0200

    iteraccion#2

--- JSBASICALGORITHMS/1.js
+++ JSBASICALGORITHMS/1.js
@@ -3,4 +3,4 @@ let x = 50;
 let h = 5; 
 let y = 10;
 let z = h + y
-console.log(z)
+console.log(z) 
---
+++ JSBASICALGORITHMS/2.js
@@ -0,0 +1,29 @@
+const character = {name: 'Jack Sparrow', age: 10};
+character.age = 25
+console.log (character.age)
+
+
+
+let firstName = 'Jon';
+let lastName = 'Snow';
+let age = 24;
+console.log("soy "+firstName +lastName, +"tengo "+age+" años y me gustan los lobos")
+console.log(`soy ${firstName} ${lastName} tengo ${age} años y me gustan los lobos`)
+
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+let totalprice = toy1.price + toy2.price
+console.log(totalprice)
+
+
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+globalBasePrice = 25000
+car1.finalprice = car1.basePrice+globalBasePrice
+car2.finalPrice = car2.basePrice+globalBasePrice
+console.log(car1.finalprice)
+console.log(car2.finalPrice)
+
+
+
