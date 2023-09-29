86cc38529470afc00e98b08fce660f941446a827
abelgh91 <abelgh91@gmail.com>
2023-09-29 17:24:45 +0200

    iteracion7

---
+++ JAVASCRIPT/neoland-basic-functions/iteracion7.js
@@ -0,0 +1,14 @@
+const nameFinder = [
+    'Peter',
+    'Steve',
+    'Tony',
+    'Natasha',
+    'Clint',
+    'Logan',
+    'Xabier',
+    'Bruce',
+    'Peggy',
+    'Jessica',
+    'Marc'
+  ];
+console.log(nameFinder.indexOf(`Steve`))
