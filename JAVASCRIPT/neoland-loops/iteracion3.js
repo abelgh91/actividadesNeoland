791ec4596c6abbd0f54ef03a8e43cd0577d11f68
abelgh91 <abelgh91@gmail.com>
2023-10-01 09:26:45 +0200

    iteracion3

--- JAVASCRIPT/neoland-loops/iteracion3.js
+++ JAVASCRIPT/neoland-loops/iteracion3.js
@@ -1,4 +1,4 @@
 const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
-for (const ciudad of placesToTravel){
-    console.log(ciudad)
+for (destino of placesToTravel) {
+    console.log(destino)
 }
