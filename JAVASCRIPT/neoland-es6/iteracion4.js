e6150386692294f02ad7e87cb78f75be94e7ffd0
abelgh91 <abelgh91@gmail.com>
2023-10-03 17:11:07 +0200

    iteracion8.js

--- JAVASCRIPT/neoland-es6/iteracion4.js
+++ JAVASCRIPT/neoland-es6/iteracion4.js
@@ -1,36 +1,36 @@
-//4.1
+4.1
 
-// const users = [
-// {id: 1, name: 'Abel'},
-// {id:2, name: 'Julia'},
-// {id:3, name: 'Pedro'},
-// {id:4, name: 'Amanda'}
-// ];
+const users = [
+{id: 1, name: 'Abel'},
+{id:2, name: 'Julia'},
+{id:3, name: 'Pedro'},
+{id:4, name: 'Amanda'}
+];
 
-// const nombres = users.map((persona) => persona.name);
+const nombres = users.map((persona) => persona.name);
 
-// console.log(nombres);
+console.log(nombres);
 
-// //4.2
+//4.2
 
-// const users = [
-// {id: 1, name: 'Abel'},
-// {id:2, name: 'Julia'},
-// {id:3, name: 'Pedro'},
-// {id:4, name: 'Amanda'}
-// ];
+const users = [
+{id: 1, name: 'Abel'},
+{id:2, name: 'Julia'},
+{id:3, name: 'Pedro'},
+{id:4, name: 'Amanda'}
+];
 
-// const nombresModificados = users.map((persona)=> persona.name.startsWith(`A`) ? `Anacleto` : persona.name)
-// console.log(nombresModificados)
+const nombresModificados = users.map((persona)=> persona.name.startsWith(`A`) ? `Anacleto` : persona.name)
+console.log(nombresModificados)
 
-// //4.3
+//4.3
 
-// const cities = [
-// 	{isVisited:true, name: 'Tokyo'}, 
-// 	{isVisited:false, name: 'Madagascar'},
-// 	{isVisited:true, name: 'Amsterdam'}, 
-// 	{isVisited:false, name: 'Seul'}
-// ];
+const cities = [
+	{isVisited:true, name: 'Tokyo'}, 
+	{isVisited:false, name: 'Madagascar'},
+	{isVisited:true, name: 'Amsterdam'}, 
+	{isVisited:false, name: 'Seul'}
+];
 
-// const citiesVisited = cities.map((city)=> city.isVisited == true ? city.name + ` (Visitado)` : city.name)
-// console.log(citiesVisited)
+const citiesVisited = cities.map((city)=> city.isVisited == true ? city.name + ` (Visitado)` : city.name)
+console.log(citiesVisited)
