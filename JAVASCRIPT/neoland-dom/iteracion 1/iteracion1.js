45705e597fd40816ed02f2f0db533f9f159b8333
abelgh91 <abelgh91@gmail.com>
2023-10-10 15:43:19 +0200

    iteracion1.js

---
+++ JAVASCRIPT/neoland-dom/iteracion 1/iteracion1.js
@@ -0,0 +1,35 @@
+//1.1
+
+let boton = document.querySelector(".showme");
+
+console.log(boton);
+
+//1.2
+
+let pillado = document.querySelector(`#pillado`);
+
+console.log(pillado);
+
+//1.3
+
+let parrafos = document.querySelector(`p`);
+
+console.log(parrafos);
+
+//1.4
+
+let pokemons = document.querySelectorAll(`.pokemon`)
+
+console.log(pokemons);
+
+//1.5
+
+let data = document.querySelectorAll('[data-function="testMe"]');
+
+console.log(data);
+
+//1.6
+
+let dataTwo = document.querySelector(`[data-function="testMe[2]"]`)
+
+console.log(data[2]);
