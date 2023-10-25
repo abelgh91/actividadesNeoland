9a32e5168f38243a3d56c161464ecc1306a58a51
abelgh91 <abelgh91@gmail.com>
2023-10-25 19:19:03 +0200

    terminado

--- NODE/Ejercicio CRUD/src/api/Routes/Character.routes.js
+++ NODE/Ejercicio CRUD/src/api/Routes/Character.routes.js
@@ -1,12 +1,16 @@
-const { create, getById, getAll, getByName } = require("../Controllers/Character.controllers");
-const express = require("express")
+const { upload } = require("../../middleware/files.middleware");
+const { create, getById, getAll, getByName, update, eliminarCharacter } = require("../Controllers/Character.controllers");
+const express = require("express");
+
 
 
 const CharactersRoutes = express.Router();
-CharactersRoutes.post("/", create);
+CharactersRoutes.post("/", upload.single("image"), create);
 CharactersRoutes.get("/:id", getById);
 CharactersRoutes.get("/", getAll);
-CharactersRoutes.get("/:name", getByName);
+CharactersRoutes.get("/byName/:name", getByName);
+CharactersRoutes.patch("/:id", upload.single("image"), update);
+CharactersRoutes.delete("/:id", eliminarCharacter);
 
 
 
