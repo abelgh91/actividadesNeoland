b2d94e2911c66a0640656c933d32747d8d368f90
abelgh91 <abelgh91@gmail.com>
2023-10-26 18:02:51 +0200

    repaso ejercicios de js

--- JAVASCRIPT/neoland-es6/iteracion2.js
+++ JAVASCRIPT/neoland-es6/iteracion2.js
@@ -24,7 +24,7 @@ const animalFunction = () => {
     return {name: 'Bengal Tiger', race: 'Tiger'}
 };
 
-const {name, race} = animalFunction()
+//  const {name, race} = animalFunction()
 console.log(name);
 console.log(race)
 
@@ -32,7 +32,7 @@ console.log(race)
 
 const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] }
 
-//const{name, itv} = car
+const{name, itv} = car
 console.log(car)
 
 const [year1, year2, year3] = itv
--- JAVASCRIPT/neoland-loops/iteracion2.js
+++ JAVASCRIPT/neoland-loops/iteracion2.js
@@ -6,13 +6,18 @@ const alumns = [
 {name: 'Lucia Aranda', T1: true, T2: false, T3: true},
 {name: 'Juan Miranda', T1: false, T2: true, T3: true},
 {name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
-{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+{name: 'Raquel Benito', T1: true, T2: true, T3: true},
 ]
+
+
 for (let i = 0; i < alumns.length; i++) {
 const alumn = alumns[i];
 const trimestresAprobados = [alumn.T1, alumn.T2, alumn.T3].filter(aprobado => aprobado).length;
 alumn.isApproved = trimestresAprobados >= 2;
 }
 console.log(alumns);
+
+
+
 
 
