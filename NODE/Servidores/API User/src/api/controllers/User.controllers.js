0add572890a5c6f7bf0f96f261697717c825c69f
abelgh91 <abelgh91@gmail.com>
2023-10-31 15:47:12 +0100

    register login autologin user

--- NODE/Servidores/API User/src/api/controllers/User.controllers.js
+++ NODE/Servidores/API User/src/api/controllers/User.controllers.js
@@ -6,6 +6,8 @@ const user = require("../models/User.model");
 const nodemailer = require("nodemailer");
 const bcrypt = require("bcrypt");
 const { generateToken } = require("../../utils/token");
+const setError = require("../../helpers/handle-error");
+const randomPassword = require("../../utils/randomPassword");
 
 
 //  ------------------REGISTER LARGO--------------------------
@@ -265,7 +267,6 @@ const sendCode = async (req, res, next) => {
   }
 };
 
-
 //----------------LOGIN----------------
 
 const login = async (req, res, next) => {
@@ -310,4 +311,246 @@ const exampleAuth = async (req,res,next)=>{
   return res.status(200).json(user)
 }
 
-module.exports = { register, registerEstado, registerWithRedirect, sendCode, login, exampleAuth };
+
+//---------------------AUTOLOGIN------------------
+
+const autoLogin = async (req, res, next) => {
+  try {
+    //recibimos el email y la password por el req.body
+    const { email, password } = req.body;
+    //buscamos un usuario con ese email
+    const userDb = await user.findOne({ email });
+
+    // vamos a ver si existe o no ese usuario
+    if (userDb) {
+      /** la diferencia con el login es que en este caso lo que comparamos son dos contraseñas encriptadas por lo que
+       * se pueden comparar directamente
+       */
+      if (password === userDb.password) {
+        /** si coincideen llamamos a la funcion que genera el token */
+        const token = generateToken(userDb._id, email);
+        // enviamos el token
+        return res.status(200).json({
+          user: userDb,
+          token,
+        });
+      } else {
+        // si las contraseñas no son iguales le mandamos un 404
+        return res.status(404).json('password dont match');
+      }
+    } else {
+      // si no existe le mando un user not register
+      return res.status(404).json('user not register');
+    }
+  } catch (error) {
+    return next(error);
+  }
+};
+
+
+//----------------------RESEND CODE-------------------
+
+const resendCode = async (req, res, next) => {
+  try {
+    const email = process.env.EMAIL;
+    const password = process.env.PASSWORD;
+    // en el transporter es donde tenemos todos los metodos necesarios para interactuar con el nodemailer
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: email,
+        pass: password,
+      },
+    });
+    // comprobar que el usuario existe 
+    const userExist = await user.findOne({ email: req.body.email });
+
+    if (userExist) { // enviamos el codigo
+      // hacemos las opciones del email
+      const mailOptions = {
+        from: email,
+        to: req.body.email,
+        subject: 'Confirmation code',
+        text: `tu codigo es ${userExist.confirmationCode}`,
+      };
+
+      // el sendMail de nodemailer para enviar el codigo y las respuestas user
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log(error);
+          return res.status(404).json({
+            resend: false,
+          });
+        } else {
+          console.log('Email sent: ' + info.response);
+          return res.status(200).json({
+            resend: true,
+          });
+        }
+      });
+    } else {
+      // si no existe le mandamos un 404 diciendo que no esta registrado
+      return res.status(404).json('User not found');
+    }
+  } catch (error) {
+    return next(
+      setError(500, error.message || 'Error general to sendResendCode')
+    );
+  }
+};
+
+//------------------CHECK NEW USER------------------------
+
+const checkNewUser = async (req, res, next) => {
+  try {
+    // recibimos el email y el codigo de confirmacion por el req.body
+    const { email, confirmationCode } = req.body;
+    // comprobamos que este usuario exista
+    const userExists = await user.findOne({ email });
+
+    // si no existe le mandamos un 404 not found
+    if (!userExists) {
+      return res.status(404).json('User not found');
+    } else {
+      // si existe comparamos el codigo de confirmacion del back y el que nos envia por el req.body
+      if (userExists.confirmationCode === confirmationCode) {
+        try {
+          // si coinciden actualizamos la clave check a true
+          await userExists.updateOne({ check: true });
+
+          // vamos a hacer actualizar buscando de nuevo el usuario actualizado para mandar el 200 y hacer un test
+          const updateUser = await user.findOne({ email });
+          return res.status(200).json({
+            testCheckUser: updateUser.check == true ? true : false,
+          });
+        } catch (error) {
+          return res.status(404).json({
+            error: 'error catch update',
+            message: error.message,
+          });
+        }
+      } else {
+        // si no coincide la password lo borramos del back
+        await user.findByIdAndDelete(userExists._id);
+        // borramos su imagen
+        deleteImgCloudinary(userExists.image);
+        // lanzamos un 404 con el usuario que antes existia, el check en false y un test de si se ha borrado correctamente o no
+        return res.status(404).json({
+          userExists,
+          check: false,
+          delete: (await user.findById(userExists._id))
+            ? 'error delete user'
+            : 'ok delete user',
+        });
+      }
+    }
+  } catch (error) {
+    return next(setError(500, error.message || 'Error general to checkCode'));
+  }
+};
+
+//--------------------CAMBIAR PASSWORD CUANDO AUN NO ESTAS LOGADO (SE TE OLVIDO LA CONTRASEÑA)
+
+const changePassword = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    // hay que buscar el usuario que exista en el back y se hace con el email que recibimos por el body
+    const userDb = await user.findOne({ email });
+
+    // si este usuario existe, me llamo a un redirect que genera la contraseña y la envia
+    if (userDb) {
+      //! importante 307 ! -----> y importante que recibe como param el id del usuario
+      return res.redirect(
+        307,
+        `http://localhost:8080/api/v1/users/sendPassword/${userDb._id}`
+      );
+    } else {
+      // si no existe le digo que este usuario no esta registrado
+      return res.status(404).json('User no register');
+    }
+  } catch (error) {
+    return next(
+      setError(500, error.message || 'Error general to changePassword NO AUTH')
+    );
+  }
+};
+
+//? REDIRECT ---------------------------SENDPASSWORD
+
+const sendPassword = async (req, res, next) => {
+  try {
+    // recuperamos el id del param para buscar el user por id
+    const { id } = req.params;
+    const userDb = await User.findById(id);
+
+    // vamos a generar la contraseña nueva mediante un util
+    const passwordSecure = randomPassword();
+
+    //! --------> ENVIO DE LA CONTRASEÑA--------
+    const email = process.env.EMAIL;
+    const password = process.env.PASSWORD;
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: email,
+        pass: password,
+      },
+    });
+
+    const mailOptions = {
+      from: email,
+      to: userDb.email,
+      subject: '-----', // no ponemos la palabra password porque gmail la elimina directamente
+      text: `User: ${userDb.name}. Your new code login is ${passwordSecure} Hemos enviado esto porque tenemos una solicitud de cambio de contraseña, si no has sido tú ponte en contacto con nosotros, gracias.`,
+    };
+    //!-------------------------------------------------------------
+    //!-------------------------------------------------------------
+    transporter.sendMail(mailOptions, async function (error, info) {
+      if (error) {
+        console.log(error);
+
+        // En caso de que haya un error en el envio- le decimos un 404 - ni te he actualizado la password ni te he enviado el correo
+        return res.status(404).json('dont send email and dont update user');
+      } else {
+        console.log('Email sent: ' + info.response);
+        // en caso de que si haya enviado el correo, haseo la contraseña (hash es encryptar)
+        const newPasswordBcrypt = bcrypt.hashSync(passwordSecure, 10);
+
+        try {
+          // despues de encryptar actualizo el user que busco por id la contraseña con la contraseña encryptada
+          await user.findByIdAndUpdate(id, { password: newPasswordBcrypt });
+
+          //todo-------------TEST PARA VER SI SE HA ACTUALIZADO CORRECTAMENTE
+          const userUpdatePassword = await User.findById(id);
+
+          //me compruebo si la contraseña generada sin encryptar es igual a la contraseña encryptada del back
+          // y lo hago cn el compareSync
+          if (bcrypt.compareSync(passwordSecure, userUpdatePassword.password)) {
+            // si son iguales directamente meto un 200
+            return res.status(200).json({
+              updateUser: true,
+              sendPassword: true,
+            });
+          } else {
+            // si no son iguales le digo te he enviado el correo pero no te he actualizado tu usuario
+            return res.status(404).json({
+              updateUser: false,
+              sendPassword: true,
+            });
+          }
+        } catch (error) {
+          return res.status(404).json({
+            error: 'error catch update',
+            message: error.message,
+          });
+        }
+      }
+    });
+  } catch (error) {
+    return next(
+      setError(500, error.message || 'Error general to sendPassword NO AUTH')
+    );
+  }
+};
+
+module.exports = { register, registerEstado, registerWithRedirect, sendCode, login, exampleAuth, autoLogin, resendCode, checkNewUser, changePassword, sendPassword };
--- NODE/Servidores/API User/src/api/routes/User.routes.js
+++ NODE/Servidores/API User/src/api/routes/User.routes.js
@@ -1,6 +1,6 @@
 const { isAuth } = require("../../middleware/auth.middleware");
 const { upload } = require("../../middleware/files.middleware");
-const {register, registerEstado, registerWithRedirect, sendCode, login, exampleAuth} = require("../controllers/User.controllers");
+const {register, registerEstado, registerWithRedirect, sendCode, login, autoLogin, exampleAuth, resendCode, checkNewUser, changePassword, sendPassword} = require("../controllers/User.controllers");
 const user = require("../models/User.model");
 
 
@@ -8,12 +8,18 @@ const user = require("../models/User.model");
 const userRoutes = require("express").Router()
 
 userRoutes.post('/register', upload.single('image'), register);
-userRoutes.post('/registerUtil', upload.single('image'), registerEstado)
-userRoutes.post('/registerRedirect',upload.single('image'), registerWithRedirect )
-userRoutes.post("/login", login)
-userRoutes.get("/pruebas", [isAuth], exampleAuth)
+userRoutes.post('/registerUtil', upload.single('image'), registerEstado);
+userRoutes.post('/registerRedirect',upload.single('image'), registerWithRedirect );
+userRoutes.post("/login", login);
+userRoutes.post('/login/autologin', autoLogin);
+userRoutes.post('/resend', resendCode);
+userRoutes.post('/check', checkNewUser);
+userRoutes.patch('/forgotpassword/forgotpassword', changePassword);
+
+//---------------controladores autenticados
+userRoutes.get("/pruebas", [isAuth], exampleAuth);
 
 // ----------> controladores que se utilizan con redirect
-userRoutes.post("/register/sendMail/:id", sendCode)
-
+userRoutes.post("/register/sendMail/:id", sendCode);
+userRoutes.patch('/sendPassword/:id', sendPassword);
 module.exports = userRoutes;
